84825189a62afe52d1361301aedd708e37afac66
ersinyldrm97 <respawm19@gmail.com>
2023-03-31 00:48:30 +0300

    pushed landing page

---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: "AnyAcademy",
+	description:
+		"Your online academy for literally everything. Millions of questions. AnyAcademy is your studying companion.",
+	url: "https://any.academy",
+	author: "Ersin Yildirim",
+	meta_data: {
+		twitter: "@ersnyldrm1",
+		opengraph_default: "/static/images/application-logo/logo-icon.png",
+	},
+	env: process.env.ELEVENTY_ENV === "production",
+};
